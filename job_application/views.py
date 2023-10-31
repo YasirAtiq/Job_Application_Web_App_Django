6c105774aa223ed8b94ad5d8a84da87f21b5398f
Yasir <DPSA00609@mypaceschool.com>
2023-10-31 22:22:31 +0400

    Getting Data from the Frontend

--- job_application/views.py
+++ job_application/views.py
@@ -1,4 +1,13 @@
 from django.shortcuts import render
+from .forms import ApplicationForm
 
 def index(request):
+    if request.method == "POST":
+        form = ApplicationForm(request.POST)
+        if form.is_valid():
+            first_name = form.cleaned_data["first name"]
+            last_name = form.cleaned_data["last name"]
+            email = form.cleaned_data["email"]
+            date = form.cleaned_data["doj"]
+            occupation = form.cleaned_data["occupation"]
     return render(request, "index.html")
