11f4f3b12f32e61f0b05f872518bd0cbe5759a9e
Yasir <DPSA00609@mypaceschool.com>
2023-11-01 16:43:03 +0400

    Creating an Admin interface.

--- job_application/admin.py
+++ job_application/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import DataBase
 
-# Register your models here.
+
+class DataBaseAdmin(admin.ModelAdmin):
+    list_display = ("first_name", "last_name", "email", "date", "occupation")
+    search_fields = ("first_name", "last_name", "email")
+    list_filter = ("date", "occupation")
+    ordering = ("first_name", )
+    readonly_fields = ("first_name", "last_name", "email", "occupation")
+
+
+admin.site.register(DataBase, DataBaseAdmin)
--- job_application/views.py
+++ job_application/views.py
@@ -8,10 +8,10 @@ def index(request):
     if request.method == "POST":
         form = ApplicationForm(request.POST)
         if form.is_valid():
-            first_name = form.cleaned_data["first name"]
-            last_name = form.cleaned_data["last name"]
+            first_name = form.cleaned_data["first_name"]
+            last_name = form.cleaned_data["last_name"]
             email = form.cleaned_data["email"]
-            date = form.cleaned_data["doj"]
+            date = form.cleaned_data["date"]
             occupation = form.cleaned_data["occupation"]
             DataBase.objects.create(first_name=first_name, 
                                     last_name=last_name, email=email, 
