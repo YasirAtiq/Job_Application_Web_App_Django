9439a07a9008b52bffe1a82981a669f893560c52
Yasir <DPSA00609@mypaceschool.com>
2023-11-16 12:30:36 +0400

    Sending email when interview canceled (a database is deleted)

--- job_application/views.py
+++ job_application/views.py
@@ -4,9 +4,8 @@ from .models import DataBase
 from django.contrib import messages
 from django.core.mail import EmailMessage
 from django.core.mail import send_mail
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
-from django.contrib.auth.models import User
 from django.conf import settings
 
 def index(request):
@@ -45,12 +44,21 @@ def about(request):
 def send_interview_date_change(sender, instance, created, **kwargs):
     if not created:
         if instance._state.db or (hasattr(instance, 'user') and instance.user and instance.user.is_staff):
-            subject = 'Interview Date Updated'
             message = f"""Dear {str(instance)},
 We have changed the date of our interview to {instance.date}. We are sorry
 for this change, if you have any queries or any difficulties to join,
 please reply to this email. Note that we may take from 2 to 3 business days to reply back.
 The Hiring Team."""
-            from_email = settings.DEFAULT_FROM_EMAIL
             to_email = instance.email
-            send_mail(subject, message, from_email, [to_email])
+            email_message = EmailMessage("Interview Date Updated", message, to=[to_email])
+            email_message.send()
+
+@receiver(post_delete, sender=DataBase)
+def send_email_when_interview_cancelled(sender, instance, **kwargs):
+    message = f"""Dear {str(instance)},
+We are very sorry to inform you that we have cancelled your interview. If you think this is a mistake
+then please reply here. Note that we may take 2 to 3 days to reply back.
+The Hiring Team."""
+    to_email = instance.email
+    email_message = EmailMessage("Interview Cancelled", message, to=[to_email])
+    email_message.send()
